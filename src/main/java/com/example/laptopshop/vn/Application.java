971e6cd6f003f63a6c616238d4190f821fd45cf0
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-09-11 13:53:32 +0700

    Off user and password security

--- src/main/java/com/example/laptopshop/vn/Application.java
+++ src/main/java/com/example/laptopshop/vn/Application.java
@@ -2,9 +2,12 @@ package com.example.laptopshop.vn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+// include: Bao gồm
+// exclude: loại bỏ
+@SpringBootApplication(
+	exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
