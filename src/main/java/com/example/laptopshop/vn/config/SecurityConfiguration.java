cc75b567f36cc74aafa8f75aaa3f7fe753b5deee
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-09-14 20:32:37 +0700

    Authorize by role

--- src/main/java/com/example/laptopshop/vn/config/SecurityConfiguration.java
+++ src/main/java/com/example/laptopshop/vn/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.example.laptopshop.vn.service.UserService;
 import com.example.laptopshop.vn.service.validator.CustomUserDetailsService;
@@ -41,19 +42,29 @@ public class SecurityConfiguration {
     }
 
     @Bean
+public AuthenticationSuccessHandler customSuccessHandler(){
+    return new CustomSuccessHandler();
+}
+
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                 .dispatcherTypeMatchers(DispatcherType.FORWARD,
                     DispatcherType.INCLUDE) .permitAll()
 
-                .requestMatchers("/","/login", "/client/**", "/css/**", "/js/**", 
+                .requestMatchers("/","/login","/product/**", "/client/**", "/css/**", "/js/**", 
                                     "/images/**").permitAll()
+                
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+
                 .anyRequest().authenticated())
 
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .failureUrl("/login?error")
+                        .successHandler(customSuccessHandler())
                         .permitAll());
         return http.build();
     }
