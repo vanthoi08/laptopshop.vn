fc28246d52064df7b97baa60bcd79ddfa09450e9
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-09-14 09:55:45 +0700

    UserDTO

--- src/main/java/com/example/laptopshop/vn/controller/Client/HomePageController.java
+++ src/main/java/com/example/laptopshop/vn/controller/Client/HomePageController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.laptopshop.vn.domain.Product;
+import com.example.laptopshop.vn.domain.dto.RegisterDTO;
 import com.example.laptopshop.vn.service.ProductService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -30,8 +35,16 @@ public class HomePageController {
     }
     @GetMapping("/register")
     public String getRegister(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
         return "client/auth/register";
     }
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO ) {
+        //TODO: process POST request
+        
+        return "client/auth/register";
+    }
+    
     
     
 }
